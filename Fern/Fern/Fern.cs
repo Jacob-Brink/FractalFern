fd7cd8c0557a0789f47dd53da7bf424570820c07
Jacob Brink <jacob@ohnopublishing.net>
2019-10-30 23:15:04 -0400

    Switched to other library

--- Fern/Fern/Fern.cs
+++ Fern/Fern/Fern.cs
@@ -11,7 +11,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Media.Animation;
 using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Drawing;
 
 namespace FernNamespace
 {
@@ -39,111 +39,20 @@ namespace FernNamespace
          * Turnbias: how likely to turn right vs. left (0=always left, 0.5 = 50/50, 1.0 = always right)
          * canvas: the canvas that the fern will be drawn on
          */
-        public Fern(double size, double redux, double turnbias, Canvas canvas)
+        public Fern(double size, double redux, double turnbias, Graphics g)
         {
-            canvas.Children.Clear();                                // delete old canvas contents
+
+            System.Drawing.Rectangle rect = new Rectangle(-100, 0, 100, 100);
+            g.FillRectangle(System.Drawing.Brushes.Black, rect);
+            g.DrawRectangle(Pens.Black, rect);
+
+            //canvas.Children.Clear();                                // delete old canvas contents
             // draw a new fern at the center of the canvas with given parameters
-            cluster((int)(canvas.Width / 2), (int)(canvas.Height / 2), size, redux, turnbias, canvas);
+            //cluster((int)(canvas.Width / 2), (int)(canvas.Height / 2), size, redux, turnbias, canvas);
         }
 
-        /*
-         * cluster draws a cluster at the given location and then draws a bunch of tendrils out in 
-         * regularly-spaced directions out of the cluster.
-         */
-        private void cluster(int x, int y, double size, double redux, double turnbias, Canvas canvas)
-        {
-            for (int i = 0; i < TENDRILS; i++)
-            {
-                // compute the angle of the outgoing tendril
-                double theta = i * 2 * Math.PI / TENDRILS;
-                tendril(x, y, size, redux, theta, 0, canvas);
-            }
-        }
 
-        /*
-         * tendril draws a tendril (a randomly-wavy line) in the given direction, for the given length, 
-         * and draws a cluster at the other end if the line is big enough.
-         */
-        private void tendril(int x1, int y1, double length, double density, double direction, int level, Canvas canvas)
-        {
-            int x2 = x1, y2 = y1;
-            Random random = new Random();
-
-            //draw current branch
-            x2 = x1 + (int)(SEGLENGTH * Math.Sin(direction));
-            y2 = y1 + (int)(SEGLENGTH * Math.Cos(direction));
-            stem(x1, y1, x2 * SCALE, y2 * SCALE, 1 + length / 5, canvas);
-
-            //base case, leaves
-            if (level >= LEVEL_MAX)
-            {
-                //leaf(x1, y1, direction, canvas);  
-                return;
-            }
-                    
-            //recursive step
-            for (int num = 0; num < 1; num++)
-            {
-                //create direction offset that is around perpendicular to branch
-                double directionOffset = random.NextDouble() % 30 +45;
-                direction = direction + directionOffset;
-
-                //draw pair of branches
-                tendril(x1, y1, length / 2, density / 2, direction, ++level, canvas);
-                tendril(x1, y1, length / 2, density / 2, direction, ++level, canvas);
-            }
-
-        }
-
-        /*
-         * draw a red circle centered at (x,y), radius radius, with a black edge, onto canvas
-         */
-        private void leaf(int x, int y, double direction, Canvas canvas)
-        {
-            Ellipse myEllipse = new Ellipse();
-            SolidColorBrush mySolidColorBrush = new SolidColorBrush();
-            mySolidColorBrush.Color = Color.FromArgb(0, 255, 0, 0);
-            myEllipse.Fill = mySolidColorBrush;
-            myEllipse.StrokeThickness = 1;
-            myEllipse.Stroke = Brushes.ForestGreen;
-            myEllipse.HorizontalAlignment = HorizontalAlignment.Center;
-            myEllipse.VerticalAlignment = VerticalAlignment.Center;
-            myEllipse.Width = 2;
-            myEllipse.Height = 2;
-            myEllipse.SetCenter(x, y);
-            canvas.Children.Add(myEllipse);
-        }
-
-        /*
-         * draw a line segment (x1,y1) to (x2,y2) with given color, thickness on canvas
-         */
-        private void stem(int x1, int y1, int x2, int y2, double thickness, Canvas canvas)
-        {
-            Line myLine = new Line();
-            SolidColorBrush mySolidColorBrush = new SolidColorBrush();
-            mySolidColorBrush.Color = Color.FromArgb(100, 0, 0, 100);
-            myLine.X1 = x1;
-            myLine.Y1 = y1;
-            myLine.X2 = x2;
-            myLine.Y2 = y2;
-            myLine.Stroke = mySolidColorBrush;
-            myLine.VerticalAlignment = VerticalAlignment.Center;
-            myLine.HorizontalAlignment = HorizontalAlignment.Left;
-            myLine.StrokeThickness = thickness;
-            canvas.Children.Add(myLine);
-        }
     }
 }
 
-/*
- * this class is needed to enable us to set the center for an ellipse (not built in?!)
- */
-public static class EllipseX
-{
-    public static void SetCenter(this Ellipse ellipse, double X, double Y)
-    {
-        Canvas.SetTop(ellipse, Y - ellipse.Height / 2);
-        Canvas.SetLeft(ellipse, X - ellipse.Width / 2);
-    }
-}
 
--- Fern/Fern/MainWindow.xaml.cs
+++ Fern/Fern/MainWindow.xaml.cs
@@ -12,6 +12,9 @@ using System.Windows.Media.Imaging;
 using System.Windows.Media.Animation;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Drawing;
+using System.IO;
+using System.Drawing.Imaging;
 
 namespace FernNamespace
 {
@@ -20,21 +23,37 @@ namespace FernNamespace
     /// </summary>
     public partial class MainWindow : Window
     {
+        //canvas is used for faster draw times
+        private Bitmap _canvas;
+
         public MainWindow()
         {
             InitializeComponent();
+            _canvas = new Bitmap((int) this.Width, (int) this.Height);
         }
 
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
-            Fern f = new Fern(sizeSlider.Value, densitySlider.Value, biasSlider.Value, canvas);
+            Graphics g = Graphics.FromImage(_canvas);
+            g.Clear(System.Drawing.Color.Red);
+            g.FillRectangle(System.Drawing.Brushes.Green, 0, 0, (int) this.Width / 2, (int) this.Height / 2);
+            BitmapImage bitmapImage = new BitmapImage();
+            bitmapImage.BeginInit();
+            MemoryStream pngStream = new MemoryStream();
+            _canvas.Save(pngStream, ImageFormat.Png);
+            pngStream.Position = 0;
+            bitmapImage.StreamSource = pngStream;
+            bitmapImage.CacheOption = BitmapCacheOption.OnLoad;
+            bitmapImage.EndInit();
+            CanvasImage.Source = bitmapImage;
         }
 
 
         private void button1_Click(object sender, RoutedEventArgs e)
         {
-            Fern f = new Fern(sizeSlider.Value, densitySlider.Value, biasSlider.Value, canvas);
+            
+
         }
     }
 
